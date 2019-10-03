9dfce94383a6301d98cd22647421aae6eaa6df64
Fardhan Ardhi <fa.dsg.id@gmail.com>
2019-10-03 13:44:55 +0700

    bundle

--- app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
@@ -2,12 +2,14 @@ package id.ac.polinema.idealbodyweight;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import id.ac.polinema.idealbodyweight.fragments.AboutFragment;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+	private AboutFragment aboutFragment;
 
 	// Deklarasikan atribut Fragment di sini
 
@@ -15,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		aboutFragment = AboutFragment.newInstance("Fardhan Ardhi");
+
+
 	}
 
 	@Override
@@ -26,6 +32,12 @@ public class MainActivity extends AppCompatActivity {
 	@Override
 	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 		// TODO: Tambahkan penanganan menu di sini
+		if (item.getItemId() == R.id.menu_about) {
+			getSupportFragmentManager().beginTransaction()
+					.replace(R.id.fragment_container, aboutFragment)
+					.addToBackStack(null)
+					.commit();
+		}
 
 		return super.onOptionsItemSelected(item);
 	}
